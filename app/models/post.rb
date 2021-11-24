c6f6e8901898dd1fba2cbcecf0052a43cd16e29d
Shivansh <shivansh18181818@gmail.com>
2021-11-24 10:49:22 +0530

    Initialize repo, Added models and controllers

---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ApplicationRecord
+  validates :content , presence: true
+
+  belongs_to :user
+
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'likes/create'
+  get 'comments/new'
+  get 'comments/create'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/create'
+  get 'users/index'
+  get 'users/show'
+  devise_for :users
+  resources :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
